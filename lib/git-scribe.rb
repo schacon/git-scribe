a89a7e5ac35701e47770456669ea2521223f7183
Scott Chacon <schacon@gmail.com>
2011-01-18 14:42:42 -0800

    can generate the example in site, html, epub, pdf

--- lib/git-scribe.rb
+++ lib/git-scribe.rb
@@ -1,3 +1,4 @@
+require 'fileutils'
 require 'pp'
 
 class GitScribe
@@ -24,6 +25,93 @@ class GitScribe
   def help
     puts "No command: #{@command}"
     puts "TODO: tons of help"
+  end
+
+  # start a new scribe directory with skeleton structure
+  def init
+  end
+
+  # check that we have everything needed
+  def check
+    # look for a2x (asciidoc, latex, xsltproc)
+  end
+
+  BOOK_FILE = 'book.asciidoc'
+
+  OUTPUT_TYPES = ['pdf', 'epub', 'mobi', 'html', 'site']
+
+  # generate the new media
+  def gen
+    type = @args.shift || 'all'
+    prepare_output_dir
+
+    gather_and_process
+
+    types = type == 'all' ? OUTPUT_TYPES : [type]
+
+    output = []
+    Dir.chdir("output") do
+      types.each do |out_type|
+        call = 'do_' + out_type
+        if self.respond_to? call
+          self.send call
+        else
+          puts "NOT A THING: #{call}"
+        end
+      end
+      # clean up
+      # `rm #{BOOK_FILE}`
+      # TODO: open media (?)
+    end
+  end
+
+  def prepare_output_dir
+    Dir.mkdir('output') rescue nil
+    Dir.chdir('output') do
+      Dir.mkdir('stylesheets') rescue nil
+      from_stdir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'stylesheets'))
+      FileUtils.cp_r from_stdir, '.'
+    end
+  end
+
+  def do_pdf
+    puts "GENERATING PDF"
+    `a2x -f pdf -d book #{BOOK_FILE}`
+    if $?.exitstatus == 0
+      'book.pdf'
+    end
+  end
+
+  def do_epub
+    puts "GENERATING EPUB"
+    `a2x -f epub -d book --epubcheck --stylesheet=stylesheets/handbookish.css #{BOOK_FILE}`
+    puts 'exit status', $?.exitstatus
+    'book.epub'
+  end
+
+  def do_html
+    puts "GENERATING HTML"
+    `a2x -f xhtml -d book --stylesheet=stylesheets/handbookish.css #{BOOK_FILE}`
+    puts 'exit status', $?.exitstatus
+    'book.html'
+  end
+
+  def do_site
+    puts "GENERATING SITE"
+    `a2x -f chunked -d book --stylesheet=stylesheets/handbookish.css #{BOOK_FILE}`
+    puts 'exit status', $?.exitstatus
+    'book.html'
+  end
+
+
+  # create a new file by concatenating all the ones we find
+  def gather_and_process
+    files = Dir.glob("book/**/*.asciidoc")
+    File.open("output/#{BOOK_FILE}", 'w+') do |f|
+      files.each do |file|
+        f.puts File.read(file)
+      end
+    end
   end
 
   # DISPLAY HELPER FUNCTIONS #
